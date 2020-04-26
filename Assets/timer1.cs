79115b17c9e5c386d55f4548a8bf1dfa2155a6d4
Simon <gruenings@gmail.com>
2020-04-26 16:20:12 +0200

    Add files via upload

---
+++ Assets/timer1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer1 : MonoBehaviour
+{
+
+    public mainstuff wow;
+
+    public double seconds;
+
+    bool enabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cam.atta
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds -= Time.deltaTime;
+        if(seconds < 0 && enabled)
+        {
+            enabled = false;
+            dothing();
+        }
+    }
+
+    void dothing()
+    {
+        wow.tankattack();
+    }
+
+}
